8b414b6a0bb7563a4d697458892cac74ad62e9e0
buchanaf <buchanaf@gmail.com>
2017-10-30 22:40:29 -0600

    Fix interceptor

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,10 @@ axios.interceptors.request.use(
   config => {
     return {
       ...config,
-      url: true === 'production' ? `https://min-api.cryptocompare.com/${config.url}` : config.url,
+      url:
+        process.env.NODE_ENV === 'production'
+          ? `https://min-api.cryptocompare.com${config.url}`
+          : config.url,
     };
   },
   error => Promise.reject(error),
